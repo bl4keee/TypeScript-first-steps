82fe2f868f9448bf3d2c98cc6f2cd282ec3327a5
Bartek Walczak <50672367+bl4keee@users.noreply.github.com>
2020-10-07 17:09:47 +0200

    Initial Commit

---
+++ 02_variables/sample-types.js
@@ -0,0 +1,8 @@
+var found = true;
+var grade = 77.7;
+var firstName = "Adam";
+var lastName = 'Kowalski';
+console.log(found);
+console.log("The grade is " + grade);
+console.log("Hi " + firstName + " " + lastName);
+console.log("Hi " + firstName + " " + lastName);
---
+++ 02_variables/sample-types.ts
@@ -0,0 +1,9 @@
+let found: boolean = true;
+let grade: number = 77.7;
+let firstName: string = "Adam";
+let lastName: string = 'Kowalski';
+
+console.log(found);
+console.log("The grade is " + grade);
+console.log("Hi " + firstName + " " + lastName);
+console.log(`Hi ${firstName} ${lastName}`);
